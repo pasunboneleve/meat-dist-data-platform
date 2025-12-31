f942ed390be567ccda7d7fa53b5edc8e8a4c996b
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2025-12-31 12:11:17 +1100

    fix the trigger script

--- scripts/trigger-ingestion.sh
+++ scripts/trigger-ingestion.sh
@@ -1,12 +1,12 @@
 #!/bin/env bash
 
-set -euox --pipefail
+set -euox pipefail
 
 #
 # This scripts triggers the synthetic meat ingestor Cloud Run service
 #
 
-
-curl -H "Authorization: Bearer $(gcloud auth print-identity-token)" \
-     $(gcloud run services describe synthetic-meat-ingestor \
+token=$(gcloud auth print-identity-token)
+url=$(gcloud run services describe synthetic-meat-ingestor \
               --region australia-southeast2 --format 'value(status.address.url)')
+curl -H "Authorization: Bearer ${token}" ${url}

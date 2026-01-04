af0aed80f72e3c14be34cab91d66ed1f9dead929
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2026-01-04 22:42:25 +1100

    output bucket names

--- warehouse/outputs.tf
+++ warehouse/outputs.tf
@@ -9,3 +9,15 @@ output "ingestion_service_name" {
 output "ingestion_service_url" {
   value = local.ingestion_service_url
 }
+
+output "bronze_bucket" {
+  value = google_storage_bucket.bronze_bucket.name
+}
+
+output "silver_bucket" {
+  value = google_storage_bucket.silver_bucket.name
+}
+
+output "gold_bucket" {
+  value = google_storage_bucket.gold_bucket.name
+}

b6bb6226db2b6a3f654448220413e9593e5c2f52
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2026-01-06 12:29:05 +1100

    memory_gb = 2.0 for Airflow 3

--- warehouse/main.tf
+++ warehouse/main.tf
@@ -263,7 +263,7 @@ resource "google_composer_environment" "meat_composer" {
       }
       triggerer {
         cpu       = 0.5
-        memory_gb = 1
+        memory_gb = 2.0
         count     = 1
       }
     }

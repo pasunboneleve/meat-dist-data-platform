c126e2e60c1958126662a65e2fcf50e9cd8ea1fa
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2026-01-08 20:24:57 +1100

    bronze_to_silver should fetch yesterday

--- dag/src/bronze_to_silver_dv2.py
+++ dag/src/bronze_to_silver_dv2.py
@@ -38,7 +38,7 @@ def get_config(**context: Dict[str, Any]) -> Dict[str, str]:
     bronze_bucket = os.environ["BRONZE_BUCKET"]
     silver_bucket = os.environ["SILVER_BUCKET"]
     deps_bucket = os.environ["DEPS_BUCKET"]
-    logical_date: date = context["logical_date"].date()  # type: ignore
+    logical_date: date = context["logical_date"].date() - timedelta(days=1)  # type: ignore
     target_date_str = logical_date.strftime("%Y/%m/%d")
     prefix = f"carcasses/**/year={logical_date.year}/month={logical_date.month:02d}/day={logical_date.day:02d}/"
     return {

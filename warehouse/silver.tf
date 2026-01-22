63402286006aa3b15e38650938056ec6484ee4dc
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2026-01-22 15:27:35 +1100

    rewrite google_bigquery_table to autodetect and use biglake conn

--- warehouse/silver.tf
+++ warehouse/silver.tf
@@ -1,179 +1,125 @@
-resource "google_biglake_table" "hub_carcass" {
-  name     = "hub_carcass"
-  database = google_biglake_database.meat_db.id
-  type     = "HIVE"
-}
-
-resource "google_bigquery_table" "bq_hub_carcass" {
+resource "google_bigquery_table" "hub_carcass" {
   project             = var.project_id
   dataset_id          = google_bigquery_dataset.silver_meat_market.dataset_id
-  table_id            = google_biglake_table.hub_carcass.name
+  table_id            = "hub_carcass"
   deletion_protection = false
 
   external_data_configuration {
-    autodetect    = true
-    source_format = "ICEBERG"
-
-    source_uris   = [google_biglake_table.hub_carcass.id]
+    autodetect            = true
+    source_format         = "ICEBERG"
+    connection_id         = google_bigquery_connection.biglake.id
+    source_uris           = ["gs://${google_storage_bucket.silver_bucket.name}/iceberg_warehouse/${local.db_name}/hub_carcass"]
   }
 }
 
-resource "google_biglake_table" "hub_plant" {
-  name     = "hub_plant"
-  database = google_biglake_database.meat_db.id
-  type     = "HIVE"
-}
-
-resource "google_bigquery_table" "bq_hub_plant" {
+resource "google_bigquery_table" "hub_plant" {
   project             = var.project_id
   dataset_id          = google_bigquery_dataset.silver_meat_market.dataset_id
-  table_id            = google_biglake_table.hub_plant.name
+  table_id            = "hub_plant"
   deletion_protection = false
 
   external_data_configuration {
-    autodetect    = true
-    source_format = "ICEBERG"
-
-    source_uris   = [google_biglake_table.hub_plant.id]
+    autodetect            = true
+    source_format         = "ICEBERG"
+    connection_id         = google_bigquery_connection.biglake.id
+    source_uris           = ["gs://${google_storage_bucket.silver_bucket.name}/iceberg_warehouse/${local.db_name}/hub_plant"]
   }
 }
 
-resource "google_biglake_table" "hub_indicator" {
-  name     = "hub_indicator"
-  database = google_biglake_database.meat_db.id
-  type     = "HIVE"
-}
-
-resource "google_bigquery_table" "bq_hub_indicator" {
+resource "google_bigquery_table" "hub_indicator" {
   project             = var.project_id
   dataset_id          = google_bigquery_dataset.silver_meat_market.dataset_id
-  table_id            = google_biglake_table.hub_indicator.name
+  table_id            = "hub_indicator"
   deletion_protection = false
 
   external_data_configuration {
-    autodetect    = true
-    source_format = "ICEBERG"
-
-    source_uris   = [google_biglake_table.hub_indicator.id]
+    autodetect            = true
+    source_format         = "ICEBERG"
+    connection_id         = google_bigquery_connection.biglake.id
+    source_uris           = ["gs://${google_storage_bucket.silver_bucket.name}/iceberg_warehouse/${local.db_name}/hub_indicator"]
   }
 }
 
-resource "google_biglake_table" "sat_carcass_detail" {
-  name     = "sat_carcass_detail"
-  database = google_biglake_database.meat_db.id
-  type     = "HIVE"
-}
-
-resource "google_bigquery_table" "bq_sat_carcass_detail" {
+resource "google_bigquery_table" "sat_carcass_detail" {
   project             = var.project_id
   dataset_id          = google_bigquery_dataset.silver_meat_market.dataset_id
-  table_id            = google_biglake_table.sat_carcass_detail.name
+  table_id            = "sat_carcass_detail"
   deletion_protection = false
 
   external_data_configuration {
-    autodetect    = true
-    source_format = "ICEBERG"
-
-    source_uris   = [google_biglake_table.sat_carcass_detail.id]
+    autodetect            = true
+    source_format         = "ICEBERG"
+    connection_id         = google_bigquery_connection.biglake.id
+    source_uris           = ["gs://${google_storage_bucket.silver_bucket.name}/iceberg_warehouse/${local.db_name}/sat_carcass_detail"]
   }
 }
 
-resource "google_biglake_table" "link_carcass_plant" {
-  name     = "link_carcass_plant"
-  database = google_biglake_database.meat_db.id
-  type     = "HIVE"
-}
-
-resource "google_bigquery_table" "bq_link_carcass_plant" {
+resource "google_bigquery_table" "link_carcass_plant" {
   project             = var.project_id
   dataset_id          = google_bigquery_dataset.silver_meat_market.dataset_id
-  table_id            = google_biglake_table.link_carcass_plant.name
+  table_id            = "link_carcass_plant"
   deletion_protection = false
 
   external_data_configuration {
-    autodetect    = true
-    source_format = "ICEBERG"
-
-    source_uris   = [google_biglake_table.link_carcass_plant.id]
+    autodetect            = true
+    source_format         = "ICEBERG"
+    connection_id         = google_bigquery_connection.biglake.id
+    source_uris           = ["gs://${google_storage_bucket.silver_bucket.name}/iceberg_warehouse/${local.db_name}/link_carcass_plant"]
   }
 }
 
-resource "google_biglake_table" "link_carcass_indicator" {
-  name     = "link_carcass_indicator"
-  database = google_biglake_database.meat_db.id
-  type     = "HIVE"
-}
-
-resource "google_bigquery_table" "bq_link_carcass_indicator" {
+resource "google_bigquery_table" "link_carcass_indicator" {
   project             = var.project_id
   dataset_id          = google_bigquery_dataset.silver_meat_market.dataset_id
-  table_id            = google_biglake_table.link_carcass_indicator.name
+  table_id            = "link_carcass_indicator"
   deletion_protection = false
 
   external_data_configuration {
-    autodetect    = true
-    source_format = "ICEBERG"
-
-    source_uris   = [google_biglake_table.link_carcass_indicator.id]
+    autodetect            = true
+    source_format         = "ICEBERG"
+    connection_id         = google_bigquery_connection.biglake.id
+    source_uris           = ["gs://${google_storage_bucket.silver_bucket.name}/iceberg_warehouse/${local.db_name}/link_carcass_indicator"]
   }
 }
 
-resource "google_biglake_table" "hub_saleyard" {
-  name     = "hub_saleyard"
-  database = google_biglake_database.meat_db.id
-  type     = "HIVE"
-}
-
-resource "google_bigquery_table" "bq_hub_saleyard" {
+resource "google_bigquery_table" "hub_saleyard" {
   project             = var.project_id
   dataset_id          = google_bigquery_dataset.silver_meat_market.dataset_id
-  table_id            = google_biglake_table.hub_saleyard.name
+  table_id            = "hub_saleyard"
   deletion_protection = false
 
   external_data_configuration {
-    autodetect    = true
-    source_format = "ICEBERG"
-
-    source_uris   = [google_biglake_table.hub_saleyard.id]
+    autodetect            = true
+    source_format         = "ICEBERG"
+    connection_id         = google_bigquery_connection.biglake.id
+    source_uris           = ["gs://${google_storage_bucket.silver_bucket.name}/iceberg_warehouse/${local.db_name}/hub_saleyard"]
   }
 }
 
-resource "google_biglake_table" "sat_saleyard_detail" {
-  name     = "sat_saleyard_detail"
-  database = google_biglake_database.meat_db.id
-  type     = "HIVE"
-}
-
-resource "google_bigquery_table" "bq_sat_saleyard_detail" {
+resource "google_bigquery_table" "sat_saleyard_detail" {
   project             = var.project_id
   dataset_id          = google_bigquery_dataset.silver_meat_market.dataset_id
-  table_id            = google_biglake_table.sat_saleyard_detail.name
+  table_id            = "sat_saleyard_detail"
   deletion_protection = false
 
   external_data_configuration {
-    autodetect    = true
-    source_format = "ICEBERG"
-
-    source_uris   = [google_biglake_table.sat_saleyard_detail.id]
+    autodetect            = true
+    source_format         = "ICEBERG"
+    connection_id         = google_bigquery_connection.biglake.id
+    source_uris           = ["gs://${google_storage_bucket.silver_bucket.name}/iceberg_warehouse/${local.db_name}/sat_saleyard_detail"]
   }
 }
 
-resource "google_biglake_table" "link_carcass_saleyard" {
-  name     = "link_carcass_saleyard"
-  database = google_biglake_database.meat_db.id
-  type     = "HIVE"
-}
-
-resource "google_bigquery_table" "bq_link_carcass_saleyard" {
+resource "google_bigquery_table" "link_carcass_saleyard" {
   project             = var.project_id
   dataset_id          = google_bigquery_dataset.silver_meat_market.dataset_id
-  table_id            = google_biglake_table.link_carcass_saleyard.name
+  table_id            = "link_carcass_saleyard"
   deletion_protection = false
 
   external_data_configuration {
-    autodetect    = true
-    source_format = "ICEBERG"
-
-    source_uris   = [google_biglake_table.link_carcass_saleyard.id]
+    autodetect            = true
+    source_format         = "ICEBERG"
+    connection_id         = google_bigquery_connection.biglake.id
+    source_uris           = ["gs://${google_storage_bucket.silver_bucket.name}/iceberg_warehouse/${local.db_name}/link_carcass_saleyard"]
   }
 }

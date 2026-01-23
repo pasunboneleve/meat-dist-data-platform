a8de7ace7ab4c9c83a5b5a1cb1156b819962425b
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2026-01-23 23:15:28 +1100

    add main() call to transform_bronze_to_silver.py

--- dag/spark_jobs/transform_bronze_to_silver.py
+++ dag/spark_jobs/transform_bronze_to_silver.py
@@ -359,3 +359,8 @@ def main():
     except Exception as e:
         logger.error(f"An unexpected error occurred: {e}", exc_info=True)
         sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
+
